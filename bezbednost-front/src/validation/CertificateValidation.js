f666b1fc9255000dc45ded960a7c4e75bd747d68
marija.petrovic <marijap@protonmail.com>
2022-05-20 13:13:01 +0200

    feat: validate certificate inputs

--- bezbednost-front/src/validation/CertificateValidation.js
+++ bezbednost-front/src/validation/CertificateValidation.js
@@ -1,9 +1,5 @@
 export const validName = new RegExp ('^[a-zA-Z]+')
 
-export const validCountry = new RegExp ('^[a-zA-Z]+')
-
-export const validEmail = new RegExp ('^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$')
-
 export const containsDangerousCharacters = (inputData) => {
     if (inputData.includes("'") || inputData.includes("\"") || inputData.includes(";") || inputData.includes("--") || inputData.includes("=")
             || inputData.includes("xp_") || inputData.includes("/*") || inputData.includes("*/") || inputData.includes("\\")) {
